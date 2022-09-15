410146d731aa8de3a0ee19f62b0f45cd332720c0
Richard Vu <richardvu.work@gmail.com>
2022-09-15 16:32:42 +0700

    Update Schema

--- src/main/java/com/enterprisesystems/erpauthenticationservice/apis/reponses/Notification.java
+++ src/main/java/com/enterprisesystems/erpauthenticationservice/apis/reponses/Notification.java
@@ -3,6 +3,7 @@ package com.enterprisesystems.erpauthenticationservice.apis.reponses;
 import java.io.Serializable;
 import java.time.ZonedDateTime;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,8 +17,10 @@ public class Notification implements Serializable {
 
   private static final long serialVersionUID = -5828101164006114538L;
   
+  @Schema(description = "Time", defaultValue = "2022-09-15T13:43:24.879068+07:00")
   private ZonedDateTime time;
 
+  @Schema(description = "Message")
   private String message;
 
 }
