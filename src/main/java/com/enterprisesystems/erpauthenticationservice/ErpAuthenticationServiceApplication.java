c97c22c4cbc95c71b6c9817633ae0cb50e7f8688
Richard Vu <richardvu.work@gmail.com>
2022-09-15 16:33:02 +0700

    Add RestTemplate

--- src/main/java/com/enterprisesystems/erpauthenticationservice/ErpAuthenticationServiceApplication.java
+++ src/main/java/com/enterprisesystems/erpauthenticationservice/ErpAuthenticationServiceApplication.java
@@ -5,6 +5,8 @@ import java.time.ZonedDateTime;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -16,6 +18,11 @@ public class ErpAuthenticationServiceApplication {
 	public static void main(String[] args) {
 		log.info("ERP Authentication Service Application starting at: {}", ZonedDateTime.now().toString());
 		SpringApplication.run(ErpAuthenticationServiceApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 
 }
